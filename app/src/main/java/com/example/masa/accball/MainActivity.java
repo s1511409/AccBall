ccbaae95c13a0b2c17deab2f8086770f9e078a71
s1511409 <s1511409@u.tsukuba.ac.jp>
2017-10-19 23:15:36 +0900

    SesorManager Completed

--- app/src/main/java/com/example/masa/accball/MainActivity.java
+++ app/src/main/java/com/example/masa/accball/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.masa.accball;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.StringBuilderPrinter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                            implements SensorEventListener{
+    SensorManager mSensorManager;
+    Sensor mAccSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mAccSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            Log.d("MainActivity",
+                    "x=" + String.valueOf(event.values[0]) +
+                    "y=" + String.valueOf(event.values[1]) +
+                    "z=" + String.valueOf(event.values[2]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
